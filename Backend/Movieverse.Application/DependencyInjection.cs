e9a465f9b8494ad024bae02c028d45ae65632a72
mbabinski218 <matebab551@student.polsl.pl>
2023-07-29 16:30:43 +0200

    Behaviors - Logging, Performance and Validation

--- Backend/Movieverse.Application/DependencyInjection.cs
+++ Backend/Movieverse.Application/DependencyInjection.cs
@@ -1,5 +1,8 @@
 ﻿using System.Reflection;
+using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Movieverse.Application.Behaviors;
 
 namespace Movieverse.Application;
 
@@ -10,7 +13,16 @@ public static class DependencyInjection
 		services.AddMediatR(cfg =>
 		{
 			cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+
+			cfg.AddRequestPreProcessor(typeof(LoggingBehavior<>));
+			
+			cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+#if _debug
+			cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+#endif
 		});
+
+		services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
 		return services;
 	}
