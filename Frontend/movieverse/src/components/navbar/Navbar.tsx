2fd663a5474267af76b560f29e56928b9286a9ee
mbabinski218 <matebab551@student.polsl.pl>
2023-09-22 23:54:24 +0200

    Navbar menu

--- Frontend/movieverse/src/components/navbar/Navbar.tsx
+++ Frontend/movieverse/src/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap";
+import { Container, Nav, Navbar as NavbarBs, Row, Col } from "react-bootstrap";
+import { useEffect, useState } from 'react';
 import Logo from "../../assets/logo.svg";
+import Chart from "../../assets/chart.svg";
 import Check from "../../assets/check.svg";
 import User from "../../assets/user.svg";
 import Menu from "../../assets/bars.svg";
@@ -7,23 +9,88 @@ import { SearchBar } from "./SearchBar";
 import "./Navbar.css";
 
 export const Navbar: React.FC = () => {
+  useEffect(() => {
+    document.title = "Movieverse"
+  })
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <NavbarBs>
-      <Container className="content">
-          <Nav.Link href="/"> 
-            <img src={Logo} className="logo" />  
-          </Nav.Link>
-          <SearchBar />
-          <div className="element button">
-            <img src={Check} className="check" />
-            <span>Watchlist</span>
-          </div>
-          <div className="element button">
-            <img src={User} className="user" />
-            <span>Sign in</span>
-          </div>          
-          <img src={Menu} className="menu" />
-      </Container>
-    </NavbarBs>
+    <>
+      <NavbarBs>
+        <Container className="content">
+            <Nav.Link href="/"> 
+              <img src={Logo} className="logo" />  
+            </Nav.Link>
+            <SearchBar />
+            <div className="element button">
+              <img src={Chart} className="chart" />
+              <span>Pro</span>
+            </div>
+            <div className="element button">
+              <img src={Check} className="check" />
+              <span>Watchlist</span>
+            </div>
+            <div className="element button">
+              <img src={User} className="user" />
+              <span>Sign in</span>
+            </div>
+            <img src={Menu} className="menu" onClick={() => {
+              setMenuOpen(!menuOpen);
+            }}/>
+        </Container>
+      </NavbarBs>
+      <div className={menuOpen ? "menu-open selected" : "menu-close"}>
+          <Container className="center">
+            <Row>
+              <Col>
+                <div className="category">
+                  <span>Movies</span>
+                </div>
+                <div className="element button">
+                  <span>Release calendar</span>
+                </div>
+                <div className="element button">
+                  <span>Top 100</span>
+                </div>
+                <div className="element button">
+                  <span>Most popular</span>
+                </div>
+                <div className="element button">
+                  <span>For you</span>
+                </div>
+              </Col>
+              <Col>
+                <div className="category">
+                  <span>Series</span>
+                </div>
+                <div className="element button">
+                  <span>Release calendar</span>
+                </div>
+                <div className="element button">
+                  <span>Top 100</span>
+                </div>
+                <div className="element button">
+                  <span>Most popular</span>
+                </div>
+                <div className="element button">
+                  <span>For you</span>
+                </div>
+              </Col>
+              <Col>
+                <div className="category">
+                  <span>Actors</span>
+                </div>
+                <div className="element button">
+                  <span>Most popular</span>
+                </div>
+                <div className="element button">
+                  <span>Born today</span>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+      </div>
+    </>          
   )
 };
