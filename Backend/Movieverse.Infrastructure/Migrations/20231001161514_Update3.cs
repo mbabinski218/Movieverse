33265c1504aead12f61b32d0dc91749712cf708a
mbabinski218 <matebab551@student.polsl.pl>
2023-10-07 17:57:30 +0200

    Home page

---
+++ Backend/Movieverse.Infrastructure/Migrations/20231001161514_Update3.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movieverse.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_Revenue",
+                table: "Statistics",
+                type: "numeric(12)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,0)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_OpeningWeekendWorldwide",
+                table: "Statistics",
+                type: "numeric(12)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,0)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_OpeningWeekendUs",
+                table: "Statistics",
+                type: "numeric(12)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,0)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_GrossWorldwide",
+                table: "Statistics",
+                type: "numeric(12)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,0)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_GrossUs",
+                table: "Statistics",
+                type: "numeric(12)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,0)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_Budget",
+                table: "Statistics",
+                type: "numeric(12)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12,0)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "BasicStatistics_Rating",
+                table: "Popularity",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "BasicStatistics_Rating",
+                table: "Medias",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(short),
+                oldType: "smallint",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "BasicStatistics_Rating",
+                table: "Episode",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(short),
+                oldType: "smallint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_Revenue",
+                table: "Statistics",
+                type: "numeric(12,0)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_OpeningWeekendWorldwide",
+                table: "Statistics",
+                type: "numeric(12,0)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_OpeningWeekendUs",
+                table: "Statistics",
+                type: "numeric(12,0)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_GrossWorldwide",
+                table: "Statistics",
+                type: "numeric(12,0)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_GrossUs",
+                table: "Statistics",
+                type: "numeric(12,0)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "BoxOffice_Budget",
+                table: "Statistics",
+                type: "numeric(12,0)",
+                precision: 12,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(12)",
+                oldPrecision: 12);
+
+            migrationBuilder.AlterColumn<short>(
+                name: "BasicStatistics_Rating",
+                table: "Popularity",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "BasicStatistics_Rating",
+                table: "Medias",
+                type: "smallint",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<short>(
+                name: "BasicStatistics_Rating",
+                table: "Episode",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+    }
+}
